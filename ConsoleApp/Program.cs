66faabb06f938ff9426752bdb1574e1d61150e16
Raychan <fasyaraihan82@gmail.com>
2026-06-04 10:46:15 +0700

    Init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = 15;
+
+for(int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.Write("foo");
+    }
+
+    if (i % 5 == 0)
+    {
+        Console.Write("bar");
+    }
+
+    if (i % 3 != 0 && i % 5 != 0)
+    {
+        Console.Write(i);
+    }
+
+    Console.Write(", ");
+}
+
