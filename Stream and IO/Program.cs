c06a6ebd69509a5123b5c43047e948d9d738d127
Raychan <fasyaraihan82@gmail.com>
2026-06-26 11:14:18 +0700

    Add new content

--- Stream and IO/Program.cs
+++ Stream and IO/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+[Serializable]
+public class User(int id, string name);
+
+[Serializable]
+public class Response(bool success, string message, User[] users);
+
+public class Program
+{
+    static void Main(String[] args)
+    {
+        string dataDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Data", "JSON");
+        
+        if (!Directory.Exists(dataDirectory))
+        {
+            Directory.CreateDirectory(dataDirectory);
+        }
+        
+        
+        // StreamWriter sw = new StreamWriter("data.json");
+        // sw.Close();
+    }    
+}
+
